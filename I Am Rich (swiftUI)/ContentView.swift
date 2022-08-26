4f319fdf65a391d00715acb34a5030fc5f1b053c
Tsahoika Hleb <hlebtsahoika@icloud.com>
2022-08-27 02:00:38 +0300

    Initial Commit

---
+++ I Am Rich (swiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich (swiftUI)
+//
+//  Created by Hleb Tsahoika on 27.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich (swiftUI)/I_Am_Rich__swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich__swiftUI_App.swift
+//  I Am Rich (swiftUI)
+//
+//  Created by Hleb Tsahoika on 27.08.22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich__swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
